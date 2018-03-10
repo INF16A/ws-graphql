a17d6e39c4f8ed356409033e16ad836c3378c90d
raspyweather <raspyweather@gmail.com>
2018-03-10 17:58:45 +0100

    ide cleanup reverted

--- src/schema.ts
+++ src/schema.ts
@@ -1,7 +1,9 @@
 import * as fs from "fs";
 import * as path from "path";
 
+console.log("scheming");
 export const schemas = fs.readdirSync(path.join(__dirname, "schema"))
     .filter(f => f.endsWith('.graphql'))
     .map(f => fs.readFileSync(path.join(__dirname, "schema", f)))
     .join('\n');
+console.log("schemed:"+schemas);
