5b8eaa590f3a76412af81b7f5782b30c481d149e
raspyweather <raspyweather@gmail.com>
2018-03-10 17:21:46 +0100

    register internship refactored, compiles now.

--- src/resolver/RootResolver.ts
+++ src/resolver/RootResolver.ts
@@ -4,10 +4,13 @@ import {Db} from "mongodb";
 import {registerCompanyResolver} from "./RegisterCompany";
 import {registerInternshipResolver} from "./RegisterInternship";
 import {Mail} from "../Mail";
+import {internshipOfferResolver} from "./InternshipOffer";
 
 export const createRootResolver = (db: Db, mail: Mail) => ({
     hello: (args, ctx) => new Greeting(ctx),
     companies: () => companyResolver(db),
     registerCompany: registerCompanyResolver(db, mail),
-    registerInternshipOffer: registerInternshipResolver(db)
-});
+    registerInternshipOffer: registerInternshipResolver(db),
+        internshipOffers: () => internshipOfferResolver(db),
+    })
+;
