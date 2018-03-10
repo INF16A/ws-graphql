d6236d94818ee005b520e69dfea6bf549635dae7
raspyweather <raspyweather@gmail.com>
2018-03-10 17:59:17 +0100

    ide cleanup reverted

--- src/authentication/authentication.ts
+++ src/authentication/authentication.ts
@@ -1,17 +1,11 @@
 import {NextFunction, Request, RequestHandler, Response} from "express";
 import {JsonWebTokenError, NotBeforeError, sign, TokenExpiredError, verify} from "jsonwebtoken";
 
-if(process.env.JWT_SECRET===undefined){
-    console.log("EMPTY:" + process.env.JWT_SECRET);
-    process.env.JWT_SECRET='m8rVNIqQ0f0pbsdYhJ9lFQ==';
-    console.log(process.env.JWT_SECRET);
-}
-
 const JWT_SECRET = Buffer.from(process.env.JWT_SECRET, 'base64');
 
 export const authenticate: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
     const token = extractToken(req.header('Authorization'));
-    if (token === null) {
+    if(token === null) {
         return next();
     }
 
@@ -19,7 +13,7 @@ export const authenticate: RequestHandler = (req: Request, res: Response, next: 
         issuer: 'prakt-graphql',
         maxAge: '30min'
     }, (err, decoded) => {
-        if (err) {
+        if(err) {
             handleTokenError(err, res);
             return;
         }
@@ -28,12 +22,8 @@ export const authenticate: RequestHandler = (req: Request, res: Response, next: 
 
         (req as any).user = decoded;
 
-        sign({username: tokenData.username}, JWT_SECRET, {
-            issuer: 'prakt-graphql',
-            subject: tokenData.sub,
-            expiresIn: '30min'
-        }, (err, newToken) => {
-            if (err) {
+        sign({username: tokenData.username}, JWT_SECRET, {issuer: 'prakt-graphql', subject: tokenData.sub, expiresIn: '30min'}, (err, newToken) => {
+            if(err) {
                 console.log('[Authorization] JWT Sign Error', err);
                 return next();
             }
@@ -44,7 +34,7 @@ export const authenticate: RequestHandler = (req: Request, res: Response, next: 
 };
 
 function extractToken(header) {
-    if (header === undefined || !header.startsWith('Bearer ')) {
+    if(header === undefined || !header.startsWith('Bearer ')) {
         return null;
     }
 
@@ -52,7 +42,7 @@ function extractToken(header) {
 }
 
 function handleTokenError(err: JsonWebTokenError | TokenExpiredError | NotBeforeError, res: Response) {
-    if (err instanceof TokenExpiredError) {
+    if(err instanceof TokenExpiredError) {
         res.status(401).json({
             error: {
                 type: "Authentication",
@@ -61,7 +51,7 @@ function handleTokenError(err: JsonWebTokenError | TokenExpiredError | NotBefore
         });
         return;
     }
-    if (err instanceof NotBeforeError) {
+    if(err instanceof NotBeforeError) {
         console.log('[Authentication] JWT not before error', err);
         res.status(403).json({
             error: {
@@ -71,7 +61,7 @@ function handleTokenError(err: JsonWebTokenError | TokenExpiredError | NotBefore
         });
         return;
     }
-    if (err instanceof JsonWebTokenError) {
+    if(err instanceof JsonWebTokenError) {
         console.log('[Authentication] JWT Error', err);
         res.status(403).json({
             error: {
