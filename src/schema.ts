4079f7fff8a1f4bf7dcf9627265e3a4fc65c587f
raspyweather <raspyweather@gmail.com>
2018-03-11 01:15:08 +0100

    debug output removed

--- src/schema.ts
+++ src/schema.ts
@@ -1,9 +1,7 @@
 import * as fs from "fs";
 import * as path from "path";
 
-console.log("scheming");
 export const schemas = fs.readdirSync(path.join(__dirname, "schema"))
     .filter(f => f.endsWith('.graphql'))
     .map(f => fs.readFileSync(path.join(__dirname, "schema", f)))
     .join('\n');
-console.log("schemed:"+schemas);
