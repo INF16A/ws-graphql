7c0ee84370c547f7ff062907fe672da5a0043f59
armbeck16 <arm.beck.16@lehre.mosbach.dhbw.de>
2018-03-13 17:31:45 +0100

    CompanyRepository refactored, company now required to create an internshipOffer

--- src/resolver/CompanyRepository.ts
+++ src/resolver/CompanyRepository.ts
@@ -3,11 +3,11 @@ import {Company} from "./Company";
 
 export class CompanyRepository {
     public static async doesCompanyExist(db: Db, id: string): Promise<Boolean> {
-        return null != await db.collection('User').findOne({role: 'company', _id: id});
+        return null != await db.collection('User').findOne({_id: id});
     }
 
-    public static async getCompanyById(db: Db, id: string): Promise<{ exists: Boolean, company: Company }> {
-        let data = await db.collection('User').findOne({role: 'company', _id: id});
+    public static async getCompanyById(db: Db, _id: string): Promise<{ exists: Boolean, company: Company }> {
+        let data = await db.collection('User').findOne({_id});
         if (data == null) {
             return {exists: false, company: null};
         }
--- src/resolver/RegisterInternship.ts
+++ src/resolver/RegisterInternship.ts
@@ -1,23 +1,37 @@
 import {Db} from "mongodb";
 import {InternshipOffer} from "./InternshipOffer";
+import {CompanyRepository} from "./CompanyRepository";
+import {UserError} from "graphql-errors";
 
 export const registerInternshipResolver = (db: Db) => async ({input}) => {
-    const doc = {
-        description: input.description,
-        jobname: input.jobname,
-        location: {
-            street: input.addressStreet,
-            plz: input.addressPLZ,
-            town: input.addressTown
-        },
-        contact: {
-            name: input.contactName,
-            email: input.contactEmail,
-            phone: input.contactPhone
-        },
-        link: input.link
-    };
-    let result = await db.collection('Internships').insertOne(doc);
-    let internshipOffer = await db.collection('Internships').findOne({_id: result.insertedId});
-    return new InternshipOffer(internshipOffer);
-};
+
+        const result = await CompanyRepository.getCompanyById(db, input.companyId);
+        if (!result.exists) {
+            throw new UserError("Can't register an internship for a non-existing company");
+        }
+        console.log(result);
+        const doc = {
+            description: input.description,
+            jobname: input.jobname,
+            location: {
+                street: input.addressStreet,
+                plz: input.addressPLZ,
+                town: input.addressTown
+            },
+            contact: {
+                name: input.contactName,
+                email: input.contactEmail,
+                phone: input.contactPhone
+            },
+            link: input.link,
+            companyId: result.company
+        };
+
+        const insertionResult = await
+            db.collection('Internships').insertOne(doc);
+        const internshipOffer = await
+            db.collection('Internships').findOne({_id: insertionResult.insertedId});
+        internshipOffer.companyId = result;
+        return new InternshipOffer(internshipOffer);
+    }
+;
